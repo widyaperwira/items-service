6ad310415a27ff3fe53d46627f684d42310c1477
Widya Perwira Utama <widyaperwira@gmail.com>
2026-01-12 21:11:03 +0700

    remove /users as a whitelist

--- src/main/java/com/example/items/config/SecurityConfig.java
+++ src/main/java/com/example/items/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/api/users/**", "/v3/api-docs/**", "/swagger-ui/**",
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**",
                                 "/swagger-ui.html")
                         .permitAll()
                         .anyRequest().authenticated())
